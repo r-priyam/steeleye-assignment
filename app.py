bb18f2f7069d3b3ebc802bccbf478b1793e991e1
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-07-04 00:48:19 +0530

    chore: finish advance filtering

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from typing import List
+from typing import List, Optional
 
 from elasticsearch import AsyncElasticsearch
 from elasticsearch.helpers import async_scan
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 
 from config import config
 from schema import Trade
@@ -75,6 +75,39 @@ async def search_trades(value: str):
     return result["hits"]["hits"][0]["_source"]
 
 
-@app.get("")
-async def filter_trades():
-    ...
+@app.get("/trades-filter/")
+async def filter_trades(
+    asset_class: Optional[str] = Query(default=""),
+    end: Optional[int] = Query(default=0),
+    max_price: Optional[int] = Query(default=0),
+    min_price: Optional[int] = Query(default=0),
+    start: Optional[int] = Query(default=0),
+    trade_type: Optional[str] = Query(default=""),
+):
+
+    result = await es.search(
+        index="steeleye",
+        query={
+            "bool": {
+                "should": [
+                    {"match": {"asset_class": asset_class}},
+                    {"match": {"trade_details.buySellIndicator": trade_type}},
+                    {
+                        "range": {
+                            "trade_date_time": {"from": str(start), "to": str(end)}
+                        }
+                    },
+                    {
+                        "range": {
+                            "trade_details.price": {"lte": min_price, "gte": max_price}
+                        }
+                    },
+                ]
+            }
+        },
+    )
+
+    if not result["hits"]["hits"]:
+        raise HTTPException(404, "No match found")
+
+    return [x["_source"] for x in result["hits"]["hits"]]
