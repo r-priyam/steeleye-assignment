22d7c9d21c2c5b6b0b951c24836c984b1550f657
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-07-03 21:38:33 +0530

    chore: add response models

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+from typing import List
+
 from elasticsearch import AsyncElasticsearch
 from elasticsearch.helpers import async_scan
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from config import config
+from schema import Trade
 
 app = FastAPI()
 es = AsyncElasticsearch(
@@ -16,18 +19,40 @@ async def app_shutdown():
     await es.close()
 
 
-@app.get("/trades")
+@app.get("/trades", response_model=List[Trade])
 async def list_trades():
-    return [x async for x in async_scan(es, index="steeleye")]
+    return [
+        x["_source"]
+        async for x in async_scan(
+            es,
+            index="steeleye",
+        )
+    ]
 
 
-@app.get("/trade/{trade_id}")
+@app.get(
+    "/trade/{trade_id}",
+    response_model=Trade,
+    responses={
+        404: {"description": "Trade not found"},
+    },
+)
 async def single_trade(trade_id: str):
     result = await es.search(index="steeleye", query={"match": {"trade_id": trade_id}})
-    return result["hits"]["hits"]
+
+    if not result["hits"]["hits"]:
+        raise HTTPException(404, "Trade not found")
+
+    return result["hits"]["hits"][0]["_source"]
 
 
-@app.get("/search-trades/{value}")
+@app.get(
+    "/search-trades/{value}",
+    response_model=Trade,
+    responses={
+        404: {"description": "Trade not found"},
+    },
+)
 async def search_trades(value: str):
     result = await es.search(
         index="steeleye",
@@ -43,7 +68,11 @@ async def search_trades(value: str):
             }
         },
     )
-    return result
+
+    if not result["hits"]["hits"]:
+        raise HTTPException(404, "Trade not found")
+
+    return result["hits"]["hits"][0]["_source"]
 
 
 @app.get("")
